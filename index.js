27d494ce53f9741c4fd9a5f1c95e6838ca36341c
Thoq <125930852+Thoq-jar@users.noreply.github.com>
2024-06-13 22:15:09 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found\n');
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
